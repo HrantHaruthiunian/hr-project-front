33cdec3ad2fd266bf8066757a8b29f4e3e1f4803
Hrant Harutyunyan <hharuthiunian@gmail.com>
2021-05-03 10:10:46 +0400

    store links

--- src/store/store.js
+++ src/store/store.js
@@ -145,8 +145,8 @@ export const apiURLS = {
    notationsAPI: "https://hr-back.herokuapp.com/v1/notations/",
 };
 
-export const apiLocalURLS {
-   usersAPI: "http://localhost:3000/v1/users/",
-   requestsAPI: "http://localhost:3000/v1/requests/",
-   notationsAPI: "http://localhost:3000/v1/notations/",
-};
+//export const apiLocalURLS {
+ //  usersAPI: "http://localhost:3000/v1/users/",
+ //  requestsAPI: "http://localhost:3000/v1/requests/",
+ //  notationsAPI: "http://localhost:3000/v1/notations/",
+//};
